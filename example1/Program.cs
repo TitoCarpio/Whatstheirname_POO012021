2c268f2cfa9cf90442ebe060e6ec4b70fff131ec
TitoCarpio <00226620@uca.edu.sv>
2021-03-12 18:13:46 -0600

    loading program

--- example1/Program.cs
+++ example1/Program.cs
@@ -6,7 +6,23 @@ namespace example1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 0, b = 0;
+
+            Console.WriteLine("Enter a value for a: ");
+            a =  Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} * {b} equals {Addnumbers(a, b)}");
+
         }
+
+        public static int Addnumbers(int a, int b)
+        {
+            return a + b;
+
+        }
+        
     }
 }
